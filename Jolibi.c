ae4282f3846629c772f9ecf20a612611f5d941aa
CodeMystique <141498914+CodeInfiltrator@users.noreply.github.com>
2024-01-03 12:26:15 +0700

    Jolibi
    Jolibi

---
+++ Jolibi.c
@@ -0,0 +1,30 @@
+//Problem Jolibi socs1
+#include <stdio.h>
+
+int main() {
+    //tc = others participants not include (Jolibi)
+    int tc;
+    int scoreJojo, scoreLili, scoreBibi, otherScore;
+    scanf("%d", &tc);
+    scanf("%d %d %d", &scoreJojo, &scoreLili, &scoreBibi);
+    for(int q = 1; q <= tc; q++) {
+        scanf("%d", &otherScore);
+    }
+        float average = ((float)(scoreBibi + scoreLili + scoreJojo + otherScore) / (tc + 3));
+        if(scoreJojo < average) {
+            printf("Jojo tidak lolos\n");
+        } else {
+            printf("Jojo lolos\n");
+        }
+        if(scoreLili < average) {
+            printf("Lili tidak lolos\n");
+        } else {
+            printf("Lili lolos\n");
+        }
+        if(scoreBibi < average) {
+            printf("Bibi tidak lolos\n");
+        } else {
+            printf("Bibi lolos\n");
+        }
+    return 0;
+}
